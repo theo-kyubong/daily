cdbefbd75b268aa883e907b73bc110b29ee4a017
Kyubong Theodore Lee <kyubonglee.sd@gmail.com>
2018-10-10 21:58:51 +0900

    Create preprocessing.R

---
+++ R/preprocessing.R
@@ -0,0 +1,7 @@
+sj_15_final <- merge(sj_15_w, sj_op_sum_15, all.x = T)
+sj_15_final %>% group_by(month=floor_date(date, "month")) %>% tally()
+sj_outputs %>% filter(year == 2016) %>% group_by(month, day) %>% summarise(ssum = sum(output))
+sj_outputs$date <- as.Date(sj_outputs$date, format = '%Y%m%d')
+sj_outputs$date <- paste0(as.character(sj_outputs$year), sj_outputs$month, sj_outputs$day)
+sj_16_w <- sj_points %>% filter(date > '2015-12-31' & date < '2017-01-01')
+sj_points <- data00 %>% filter(point == 133)
